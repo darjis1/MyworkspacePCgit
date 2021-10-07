9d3a12cfbf6eac55596de39d10222f2c27330044
darjis1 <92122043+darjis1@users.noreply.github.com>
2021-10-07 16:04:10 -0400

    Add files via upload

---
+++ printbackwards.py
@@ -0,0 +1,11 @@
+#Shrey Darji    9/16/21 Phrasebackwards
+print('Welcome to printbackwards or sdrawkcabtnip') 
+phrase = input('Enter your phrase here:') 
+phrasebackwards = phrase[::-1]  
+print("Your phrase backwards is:", phrasebackwards) 
+
+
+
+
+
+
